d2400f3895c4df7b12946a39e275831e40263fa0
Pengfei Xu <34528721+xupengfei1009@users.noreply.github.com>
2024-12-24 18:58:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import pickle
 from sklearn.preprocessing import OneHotEncoder
 
